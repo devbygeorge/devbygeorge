bd3c3b97d5f3b43513310297d9abbaf061d85d6f
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-11-22 10:19:31 +0400

    Build header and footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,45 @@
-import React from "react";
 import s from "./Footer.module.scss";
 
 type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div id="footer" className={s.footer}>
-      I'm a Header
-    </div>
+    <footer id="footer" className={s.footer}>
+      <ul className={s.list}>
+        <li className={s.item}>find me in:</li>
+        <li className={s.item}>
+          <a
+            className={s.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://facebook.com"
+          >
+            <i className="ri-facebook-fill social"></i>
+          </a>
+        </li>
+        <li className={s.item}>
+          <a
+            className={s.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/giorgipasieshvili"
+          >
+            <i className="ri-linkedin-fill social"></i>
+          </a>
+        </li>
+        <li className={`${s.item} ${s.emptyItem}`}></li>
+        <li className={s.item}>
+          <a
+            className={s.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/GiorgiPasieshvili"
+          >
+            @GiorgiPasieshvili
+            <i className="ri-github-fill"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import { useState } from "react";
+import Link from "next/link";
 import s from "./Header.module.scss";
+import "remixicon/fonts/remixicon.css";
 
 type Props = {};
 
 export default function Header({}: Props) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div id="header" className={s.header}>
-      I'm a Header
-    </div>
+    <header id="header" className={s.header}>
+      <nav className={s.nav}>
+        {/* Navigate Home */}
+        <div className={s.home}>
+          <Link href="/">giorgi-pasieshvili</Link>
+
+          {/* Toggle Button */}
+          <button
+            onClick={() => setMenuOpen((state) => !state)}
+            className={`${s.toggle} ${isMenuOpen ? s.toggleActive : ""}`}
+          >
+            <i className="ri-menu-line"></i>
+            <i className="ri-close-line"></i>
+          </button>
+        </div>
+
+        {/* Menu List */}
+        <ul className={`${s.list} ${isMenuOpen ? s.listActive : ""}`}>
+          <li className={s.item}>
+            <Link className={s.link} href="/">
+              _hello
+            </Link>
+          </li>
+          <li className={s.item}>
+            <Link className={s.link} href="/about">
+              _about-me
+            </Link>
+          </li>
+          <li className={s.item}>
+            <Link className={s.link} href="/projects">
+              _projects
+            </Link>
+          </li>
+          <li className={`${s.item} ${s.emptyItem}`}></li>
+          <li className={s.item}>
+            <Link className={s.link} href="/contact">
+              _contact-me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
