0f0fd40cd9bac4ffda83d806e108f00a3701aca1
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-01-10 18:12:40 +0400

    Update sanity packages and add correct projects types

--- sanity/schemas/project.js
+++ sanity/schemas/project.js
@@ -1,30 +1,62 @@
 export default {
-    name: "project",
-    title: "Project",
-    type: "document",
-    fields: [
-      {
-        name: "category",
-        title: "Category",
-        type: "string",
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "category",
+      title: "Category",
+      type: "string",
+    },
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
       },
-      {
-        name: "title",
-        title: "Title",
-        type: "string",
-      },
-      {
-        name: "description",
-        title: "Description",
-        type: "string",
-      },
-      {
-        name: "image",
-        title: "Image",
-        type: "image",
-        options: {
-          hotspot: true,
+    },
+    {
+      name: "demo",
+      title: "Demo Url",
+      type: "string",
+    },
+    {
+      name: "github",
+      title: "Github Url",
+      type: "string",
+    },
+    {
+      title: "Tech",
+      name: "tech",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              title: "Name",
+              name: "name",
+              type: "string",
+            },
+            {
+              title: "Slug",
+              name: "slug",
+              type: "string",
+            },
+          ],
         },
-      },
-    ],
-  };
+      ],
+    },
+  ],
+};
