55d992cef4bddd76c841ba5d0a1a0b026988007a
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-01-11 16:04:08 +0400

    Update PageInfo schema / add about field

--- sanity/schemas/pageInfo.js
+++ sanity/schemas/pageInfo.js
@@ -1,43 +1,12 @@
 export default {
-    name: "pageInfo",
-    title: "PageInfo",
-    type: "document",
-    fields: [
-      {
-        name: "heroTitle",
-        title: "HeroTitle",
-        type: "string",
-      },
-      {
-        name: "heroImage",
-        title: "HeroImage",
-        type: "image",
-        options: {
-          hotspot: true,
-        },
-      },
-      {
-        name: "aboutTitle",
-        title: "AboutTitle",
-        type: "string",
-      },
-      {
-        name: "aboutInfo",
-        title: "AboutInfo",
-        type: "string",
-      },
-      {
-        name: "aboutImage",
-        title: "AboutImage",
-        type: "image",
-        options: {
-          hotspot: true,
-        },
-      },
-      {
-        name: "contactText",
-        title: "ContactText",
-        type: "string",
-      },
-    ],
-  };
+  name: "pageInfo",
+  title: "PageInfo",
+  type: "document",
+  fields: [
+    {
+      name: "about",
+      title: "About",
+      type: "text",
+    },
+  ],
+};
