6c3f5c467f8a487e2db2e149297337131f28e8a2
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-05-04 16:29:41 +0400

    Replace vercel favicon with the real one

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome - Giorgi Pasieshvili</title>
         <meta name="description" content="Portfolio Giorgi Pasieshvili" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.png" />
       </Head>
 
       <Component {...pageProps} />
