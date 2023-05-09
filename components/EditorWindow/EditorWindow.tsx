7bc81dab3e2277a9380211ab1711742eef862546
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-05-10 03:56:17 +0400

    Remove automatic resizing of about paragraph

--- components/EditorWindow/EditorWindow.tsx
+++ components/EditorWindow/EditorWindow.tsx
@@ -8,21 +8,9 @@ type Props = {
 };
 
 export default function EditorWindow({ about }: Props) {
-  const [maxCharacters, setMaxCharacters] = useState(
-    Math.floor(50 / window.devicePixelRatio)
-  );
+  const [maxCharacters, setMaxCharacters] = useState(50);
   const newRegexp = new RegExp(`.{0,${maxCharacters}}\\b`, "g");
   const splittedParagraph = about.match(newRegexp);
-
-  useEffect(() => {
-    function handleResize() {
-      const maxCharacters = Math.floor(50 / window.devicePixelRatio);
-      setMaxCharacters(maxCharacters);
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <div className={s.editorWindow}>
